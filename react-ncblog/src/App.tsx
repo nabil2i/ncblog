eb225a8b9d7229d1d3590d5275336df5ec1215c0
Nabil Affo <nabil.noble01@gmail.com>
2023-06-11 11:22:44 +0000

    Modify NavButtons

--- react-ncblog/src/App.tsx
+++ react-ncblog/src/App.tsx
@@ -1,29 +1,45 @@
-import { Grid, GridItem, Show } from '@chakra-ui/react'
-import NavLogo from './components/NavLogo'
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavLogo from "./components/NavLogo";
+import NavButtons from "./components/NavButtons";
 
 function App() {
   return (
     <>
-      <Grid templateAreas={ 
-        /* `"nav-logo  nav-menu nav-search" "main main aside"` */
-        {
-        base: `"nav-logo" "main"`,
-         lg: `"nav-logo  nav-menu nav-search" "main main aside"`
-        // lg: `"nav" "main"`
-        }  
-      }> 
+      <Grid
+        templateAreas={
+          /* `"nav-logo  nav-menu nav-search" "main main aside"` */
+          {
+            base: `"search search" "nav-logo nav-buttons" "main main"`,
+            lg: `"search search search" "nav-logo  nav-menu nav-buttons" "main main aside"`,
+            // lg: `"nav" "main"`
+          }
+        }
+      >
         {/* <GridItem area='nav' bg='coral'>Nav</GridItem> */}
-        <GridItem area='nav-logo' bg='coral'><NavLogo></NavLogo></GridItem>
-        <GridItem area='main' bg='dodgerblue'>Main</GridItem>
+        <GridItem area="search" bg="green">
+          Search
+        </GridItem>
+        <GridItem area="nav-logo">
+          <NavLogo></NavLogo>
+        </GridItem>
+        <GridItem area="main" bg="dodgerblue">
+          Main
+        </GridItem>
+        <GridItem area="nav-buttons" >
+          <NavButtons></NavButtons>
+        </GridItem>
         <Show above="lg">
-          <GridItem area='nav-menu' bg='red'>Menu</GridItem>
-          <GridItem area='nav-search' bg='grayal'>Search</GridItem>
-          <GridItem area='aside' bg='gold'>Aside</GridItem>
+          <GridItem area="nav-menu" bg="red">
+            Menu
+          </GridItem>
+
+          <GridItem area="aside" bg="gold">
+            Aside
+          </GridItem>
         </Show>
-        
       </Grid>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ react-ncblog/src/components/NavButtons.tsx
@@ -0,0 +1,21 @@
+import { Search2Icon } from "@chakra-ui/icons";
+import { Flex, HStack, Spacer, Text } from "@chakra-ui/react";
+import ColorModeSwitch from "./ColorModeSwitch";
+
+const NavButtons = () => {
+  return (
+    <Flex alignItems="right" gap="2">
+      <Spacer />
+      <HStack justifyContent="space-between" padding="10px">
+        <HStack>
+          <Search2Icon boxSize={6} />
+          <Text>Search</Text>
+        </HStack>
+
+        <ColorModeSwitch />
+      </HStack>
+    </Flex>
+  );
+};
+
+export default NavButtons;

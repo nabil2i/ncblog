7d129d087f9b899946533a518845de6a26879e32
Nabil Affo <nabil.noble01@gmail.com>
2023-06-14 14:45:30 +0000

    backend - create model for posts

---
+++ node-ncblog/server/models/post.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+
+const postSchema = new mongoose.Schema({
+  title: {
+    type: string,
+    required: true,
+    minlength: 5,
+    maxlength: 255
+  },
+  body: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+  createAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Post = mongoose.model('Post', postSchema);
+
+function validatePost(post) {
+  const schema = Joi.object({
+    title: Joi.string().min(5).max(255).required(),
+    body: Joi.string().min(5).required()
+  });
+
+  return schema.validate(post);
+}
+
+exports.Post = Post;
+exports.validate = validatePost;
+exports.postSchema = postSchema;

716a720dfc0f880dcf913da4e0385f4abe38be68
Nabil Affo <nabil.noble01@gmail.com>
2023-07-15 18:14:00 +0000

    handle errors for the pages

--- react-ncblog/src/routes.tsx
+++ react-ncblog/src/routes.tsx
@@ -3,11 +3,13 @@ import HomePage from "./pages/HomePage";
 import Layout from "./pages/Layout";
 import BlogPage from "./pages/BlogPage";
 import PostPage from "./pages/PostPage";
+import ErrorPage from "./pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Layout/>,
+    errorElement: <ErrorPage/>,
     children: [
       { index: true, element: <HomePage/>},
       { path: 'blog', element: <BlogPage/>},

9e1a62af83c69c8ec85ef9728de8bbf27509ba6a
Nabil Affo <nabil.noble01@gmail.com>
2023-09-27 13:59:27 +0000

    remove unnecessary comment in layout

--- react-ncblog/src/pages/Layouts/Layout.tsx
+++ react-ncblog/src/pages/Layouts/Layout.tsx
@@ -3,13 +3,6 @@ import { Outlet } from "react-router-dom";
 import { AuthProvider, NavBar } from "../../components/navigationbar";
 
 const Layout = () => {
-  // const [userData, dispatch] = useReducer(authReducer, {});
-  // const setLatestPosts = usePostQueryStore(s => s.setLatestPosts);
-  // const location = useLocation();
-  // console.log(`Current location: ${location.pathname}`)
-  // if (location.pathname === '/') {
-  //   setLatestPosts(3)
-  // } else {setLatestPosts(0)}
 
   return (
     <AuthProvider>

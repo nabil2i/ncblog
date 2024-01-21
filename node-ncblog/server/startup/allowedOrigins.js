ca068f18d2a2d4787cc12b7a1d4980638432db7e
Nabil Affo <nabil.noble01@gmail.com>
2024-01-21 13:46:35 +0000

    fix readm file

--- node-ncblog/server/startup/allowedOrigins.js
+++ node-ncblog/server/startup/allowedOrigins.js
@@ -5,6 +5,7 @@ const allowedOrigins = [
   'http://127.0.0.1:5173',
   'http://127.0.0.1:5000',
   'http://127.0.0.1:3000',
+  process.env.NODE_APP_FRONTEND_DOMAIN
 ]
 
 module.exports = allowedOrigins

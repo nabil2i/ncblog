ac17f21ca8a585aa84194ef6b57eb59dfd5de0ae
Nabil Affo <nabil.noble01@gmail.com>
2024-02-06 10:11:44 +0000

    refactor firebase env vars

--- react-ncblog/src/firebase.ts
+++ react-ncblog/src/firebase.ts
@@ -7,15 +7,25 @@ import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+
+const apiKey = import.meta.env.DEV ? import.meta.env.VITE_FIREBASE_API_KEY : process.env.FIREBASE_API_KEY;
+const authDomain = import.meta.env.DEV ? import.meta.env.VITE_FIREBASE_AUTH_DOMAIN : process.env.FIREBASE_AUTH_DOMAIN;
+const projectId = import.meta.env.DEV ? import.meta.env.VITE_FIREBASE_PROJECT_ID : process.env.FIREBASE_PROJECT_ID;
+const storageBucket = import.meta.env.DEV ? import.meta.env.VITE_FIREBASE_STORAGE_BUCKET : process.env.FIREBASE_STORAGE_BUCKET;
+const messagingSenderId = import.meta.env.DEV ? import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID : process.env.FIREBASE_MESSAGING_SENDER_ID;
+const appId = import.meta.env.DEV ? import.meta.env.VITE_FIREBASE_APP_ID : process.env.FIREBASE_APP_ID;
+
+
+
 // console.log(import.meta.env.VITE_FIREBASE_APP_ID)
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
   // apiKey: "AIzaSyDQWWDM2uCTBF6h7VvCw9f_uRfA8YdW4G0",
   // authDomain: "ncblog-b9e81.firebaseapp.com",
   // projectId: "ncblog-b9e81",

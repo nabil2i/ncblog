8621cf287e303f4d5b1536fe08b70311f464141a
Nabil Affo <nabil.noble01@gmail.com>
2024-02-06 14:22:54 +0000

    edit home page

--- react-ncblog/src/pages/common/HomePage.tsx
+++ react-ncblog/src/pages/common/HomePage.tsx
@@ -1,24 +1,24 @@
 import { Box, Grid, GridItem } from "@chakra-ui/react";
-import CallToActionOneReason from "../../components/common/CallToActionOneReason";
+import image4 from "../../assets/images/hero-image.webp";
+import image2 from "../../assets/images/landscape.jpeg";
+import image1 from "../../assets/images/login.jpg";
+import image5 from "../../assets/images/pof.jpg";
+import image3 from "../../assets/images/pol.jpg";
 import HeroSection from "../../components/common/HeroSection";
+import ImageCarousel from "../../components/common/ImageCarousel";
 import LatestPosts from "../../components/posts/LatestPosts";
-// import image4 from "../../assets/images/hero-image.webp";
-// import image2 from "../../assets/images/landscape.jpeg";
-// import image1 from "../../assets/images/login.jpg";
-// import image5 from "../../assets/images/pof.jpg";
-// import image3 from "../../assets/images/pol.jpg";
-// import ImageCarousel from "../../components/common/ImageCarousel";
+// import CallToActionOneReason from "../../components/common/CallToActionOneReason";
 
 const HomePage = () => {
   // const setPage = usePostQueryStore((s) => s.setPage);
   // const searchText = usePostQueryStore((s) => s.postQuery.searchText);
-  // const images: Array<{ url: string; caption: string }> = [
-  //   { url: image1, caption: "Image 1" },
-  //   { url: image2, caption: "Image 2" },
-  //   { url: image3, caption: "Image 3" },
-  //   { url: image4, caption: "Image 4" },
-  //   { url: image5, caption: "Image 5" },
-  // ];
+  const images: Array<{ url: string; caption: string }> = [
+    { url: image1, caption: "Image 1" },
+    { url: image2, caption: "Image 2" },
+    { url: image3, caption: "Image 3" },
+    { url: image4, caption: "Image 4" },
+    { url: image5, caption: "Image 5" },
+  ];
 
   return (
     <Box as="main">
@@ -29,13 +29,13 @@ const HomePage = () => {
         <GridItem area="main">
           <Box>
             <HeroSection />
-            {/* <Box as="section">
+            <Box as="section">
               <ImageCarousel images={images} />
-            </Box> */}
+            </Box>
             <Box maxW="1440px" mx="auto" w="full">
               <LatestPosts />
               <Box m={5}>
-                <CallToActionOneReason />
+                {/* <CallToActionOneReason /> */}
               </Box>
             </Box>
           </Box>

4d17612c35785aeeb366effcb5cffaf530aafe04
Nabil Affo <nabil.noble01@gmail.com>
2024-02-06 10:44:21 +0000

    edit vite config

--- react-ncblog/vite.config.ts
+++ react-ncblog/vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
 import dotenv from 'dotenv';
+import { defineConfig } from 'vite';
 
 dotenv.config(); // Load environment variables from .env file
 
@@ -18,9 +18,14 @@ export default defineConfig({
     include: "**/*.tsx",
   })],
   define: {
-    // "process.env.API_BASE_URL": `"${process.env.API_BASE_URL}"`,
-    // "process.env.VITE_FIREBASE_API_KEY": `"${process.env.VITE_FIREBASE_API_KEY}"`,
-    // 'process.env.API_BASE_URL': JSON.stringify(process.env.API_BASE_URL),
+    'process.env.API_BASE_URL': JSON.stringify(process.env.API_BASE_URL),
+    'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+    'process.env.FIREBASE_AUTH_DOMAIN' : JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+    'process.env.FIREBASE_PROJECT_ID' : JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+    'process.env.FIREBASE_STORAGE_BUCKET' : JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+    'process.env.FIREBASE_MESSAGING_SENDER_ID' : JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+    'process.env.FIREBASE_APP_ID' : JSON.stringify(process.env.FIREBASE_APP_ID),
+
   },
   resolve: {
     alias: {

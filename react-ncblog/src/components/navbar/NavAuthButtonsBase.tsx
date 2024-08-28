a2f08ac0806df18bca51308adb71caab97db5932
Nabil Affo <nabil.noble01@gmail.com>
2024-08-28 11:42:48 +0000

    fix navauthbuttonsbase margin issue
    allow margin only when user is not authenticated as we display the navauthbuttons base in that case

--- react-ncblog/src/components/navbar/NavAuthButtonsBase.tsx
+++ react-ncblog/src/components/navbar/NavAuthButtonsBase.tsx
@@ -1,22 +1,21 @@
-import { Divider, Flex, useBreakpointValue } from "@chakra-ui/react";
+import { Divider, Flex } from "@chakra-ui/react";
+import { useSelector } from "react-redux";
+import { authSatus } from "../../app/features/auth/authSlice";
 import NavAuthButtons from "./NavAuthButtons";
 
 const NavAuthButtonsBase = () => {
-  const showNavAuthButtonsOnBase = useBreakpointValue({ base: true, lg: false });
+  const isAuthenticated = useSelector(authSatus);
 
   return (
     <>
-      { showNavAuthButtonsOnBase && (
       <Flex w="full" direction="column">
-        <Flex w="full" justify="end" my={2} mr={2}>
+        <Flex w="full" justify="end"  my={ isAuthenticated ? 0 : 2 } mr={2}>
           <NavAuthButtons />
         </Flex>
         <Flex w="full">
-          <Divider orientation="horizontal" />
+          {!isAuthenticated && <Divider orientation="horizontal" />}
         </Flex>
       </Flex>
-
-    )}
     </>
   );
 };
--- react-ncblog/src/components/navbar/NavBar.tsx
+++ react-ncblog/src/components/navbar/NavBar.tsx
@@ -32,119 +32,121 @@ const NavBar = () => {
     dispatch({ type: "SET_IS_OPEN", isOpen: isOpen });
   }, [dispatch, isOpen, onClose]);
 
-  const showNavAuthButtonsOnLarge = useBreakpointValue({ base: false, lg: true });
- 
-  return (
-    <>
-      <Box
-        as="header"
-        position="fixed"
-        top="0"
-        backgroundColor={useColorModeValue(
-          "rgba(255, 255, 255, 1)",
-          "rgba(0, 0, 0, 1)"
-        )}
-        w="full"
-        zIndex="999"
-        // backdropFilter="saturate(180%) blur(5px)"
-        // maxWidth={"1000px"}
-        // boxShadow="0 1px 2px 0 rgba(0, 0, 0, 0.05)"
-        borderBottomWidth={1}
-        borderColor="gray.100"
-      >
-        <NavAuthButtonsBase />
+  const showNavAuthButtonsOnLarge = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
+  // const showNavAuthButtonsOnBase = useBreakpointValue({ base: true, lg: false });
 
+  return (
+    <Box
+      as="header"
+      position="fixed"
+      top="0"
+      backgroundColor={useColorModeValue(
+        "rgba(255, 255, 255, 1)",
+        "rgba(0, 0, 0, 1)"
+      )}
+      w="full"
+      zIndex="999"
+      // backdropFilter="saturate(180%) blur(5px)"
+      // maxWidth={"1000px"}
+      // boxShadow="0 1px 2px 0 rgba(0, 0, 0, 0.05)"
+      borderBottomWidth={1}
+      borderColor="gray.100"
+    >
+      {!showNavAuthButtonsOnLarge && <NavAuthButtonsBase />}
+
+      <Flex
+        as="nav"
+        px={{ base: 4 }}
+        minH={"60px"}
+        // bg={useColorModeValue('white', 'gray.800')}
+        // color={useColorModeValue('gray.600', 'white')}
+        // py={{ base: 2 }}
+        // borderBottom={1}
+        // borderStyle={'solid'}
+        // boxShadow={'dark-lg'}
+        // borderColor={useColorModeValue('gray.200', 'gray.900')}
+        align={"center"}
+        justify="space-between"
+        w="full"
+        h="full"
+      >
+        {/* START Logo */}
         <Flex
-          as="nav"
-          px={{ base: 4 }}
-          minH={"60px"}
-          // bg={useColorModeValue('white', 'gray.800')}
-          // color={useColorModeValue('gray.600', 'white')}
-          // py={{ base: 2 }}
-          // borderBottom={1}
-          // borderStyle={'solid'}
-          // boxShadow={'dark-lg'}
-          // borderColor={useColorModeValue('gray.200', 'gray.900')}
-          align={"center"}
-          justify="space-between"
-          w="full"
+          align="center"
+          // paddingRight={4}
+        >
+          <Box>
+            {" "}
+            <NavLogo boxSize="30px" />{" "}
+          </Box>
+        </Flex>
+        {/* END Logo */}
+
+        {/* START Desktop menu */}
+        <Flex
+          display={{ base: "none", lg: "flex" }}
+          // align={"center"}
           h="full"
         >
-          {/* START Logo */}
-          <Flex
-            align="center"
-            // paddingRight={4}
-          >
-            <Box>
-              {" "}
-              <NavLogo boxSize="30px" />{" "}
-            </Box>
-          </Flex>
-          {/* END Logo */}
-
-          {/* START Desktop menu */}
-          <Flex
-            display={{ base: "none", lg: "flex" }}
-            // align={"center"}
-            h="full"
-          >
-            <DesktopNav />
-          </Flex>
-          {/* END Desktop logo and menu */}
-
-          {/* START Search */}
-          <Box
-            px={8}
-            flexGrow="1"
-            display={{ base: "none", md: "flex", lg: "none" }}
-          >
-            <SearchInput />
-          </Box>
-          {/* END Search */}
-
-          {/* START right end of menu */}
-          <Stack align="center" direction={"row"} gap={2}>
-            <Box display={{ base: "flex", md: "none", lg: "flex" }}>
-              <SearchInputModalIcon />
-            </Box>
-            <ColorModeSwitch />
-            {showNavAuthButtonsOnLarge && <NavAuthButtons />}
-            <Profile />
-
-            {/* START MobileNav */}
-            <Box
-              // py={{ base: 2 }}
-              // paddingLeft={{ base: 4 }}
-              display={{ lg: "none" }}
-            >
-              <IconButton
-                onClick={onToggle}
-                icon={<HamburgerIcon w={7} h={7} />}
-                variant={"ghost"}
-                aria-label={"Toggle Navigation"}
-              />
-            </Box>
-            <Drawer
-              isOpen={isOpen}
-              placement="left"
-              isFullHeight
-              size="full"
-              onClose={onClose}
-
-              // finalFocusRef={btnRef}
-            >
-              <Box p={0} m={0}>
-                <Show above="base" below="lg">
-                  <MobileNavDrawer />
-                </Show>
-              </Box>
-            </Drawer>
-            {/* END MobileNav */}
-          </Stack>
-          {/* END right end of menu */}
+          <DesktopNav />
         </Flex>
-      </Box>
-    </>
+        {/* END Desktop logo and menu */}
+
+        {/* START Search */}
+        <Box
+          px={8}
+          flexGrow="1"
+          display={{ base: "none", md: "flex", lg: "none" }}
+        >
+          <SearchInput />
+        </Box>
+        {/* END Search */}
+
+        {/* START right end of menu */}
+        <Stack align="center" direction={"row"} gap={2}>
+          <Box display={{ base: "flex", md: "none", lg: "flex" }}>
+            <SearchInputModalIcon />
+          </Box>
+          <ColorModeSwitch />
+          {showNavAuthButtonsOnLarge && <NavAuthButtons />}
+          <Profile />
+
+          {/* START MobileNav */}
+          <Box
+            // py={{ base: 2 }}
+            // paddingLeft={{ base: 4 }}
+            display={{ lg: "none" }}
+          >
+            <IconButton
+              onClick={onToggle}
+              icon={<HamburgerIcon w={7} h={7} />}
+              variant={"ghost"}
+              aria-label={"Toggle Navigation"}
+            />
+          </Box>
+          <Drawer
+            isOpen={isOpen}
+            placement="left"
+            isFullHeight
+            size="full"
+            onClose={onClose}
+
+            // finalFocusRef={btnRef}
+          >
+            <Box p={0} m={0}>
+              <Show above="base" below="lg">
+                <MobileNavDrawer />
+              </Show>
+            </Box>
+          </Drawer>
+          {/* END MobileNav */}
+        </Stack>
+        {/* END right end of menu */}
+      </Flex>
+    </Box>
   );
 };
 

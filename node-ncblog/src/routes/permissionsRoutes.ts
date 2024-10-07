449adf8666569dcad73ad4995b4837a28fd1c4bb
Nabil Affo <nabil.noble01@gmail.com>
2024-10-07 18:35:53 +0000

    fix editor line height

--- node-ncblog/src/routes/permissionsRoutes.ts
+++ node-ncblog/src/routes/permissionsRoutes.ts
@@ -6,7 +6,7 @@ import checkRole from '../middleware/checkRole.js';
 const router = express.Router();
 
 router.route('/')
-  .get([auth as RequestHandler], getAllPermissions);
-  // .get([auth as RequestHandler, checkRole(['superadmin'])], getAllPermissions);
+  // .get([auth as RequestHandler], getAllPermissions);
+  .get([auth as RequestHandler, checkRole(['superadmin'])], getAllPermissions);
 
 export default router;
--- node-ncblog/src/routes/rolesRoutes.ts
+++ node-ncblog/src/routes/rolesRoutes.ts
@@ -1,16 +1,18 @@
-import express from 'express';
+import express, { RequestHandler } from 'express';
 import { deletePermission, updatePermission } from '../controllers/permissionsController.js';
 import { createNewRole, getAllRoles } from '../controllers/rolesController.js';
-import paginate from '../middleware/paginateWithPage.js';
+import auth from '../middleware/auth.js';
+import checkRole from '../middleware/checkRole.js';
+import paginateWithPage from '../middleware/paginateWithPage.js';
 import RoleModel from '../models/role.js';
 
 const router = express.Router();
 
 router.route('/')
-  .get(paginate(RoleModel), getAllRoles)
-  // .get([auth as RequestHandler, checkRole(['superadmin'])], getAllRoles);
-  .post(createNewRole)
-  .put(updatePermission)
-  .delete(deletePermission)
+  // .get(paginate(RoleModel), getAllRoles)
+  .get([auth as RequestHandler, checkRole(['superadmin'])], paginateWithPage(RoleModel), getAllRoles)
+  .post([auth as RequestHandler, checkRole(['superadmin'])], createNewRole)
+  .put([auth as RequestHandler, checkRole(['superadmin'])], updatePermission)
+  .delete([auth as RequestHandler, checkRole(['superadmin'])], deletePermission);
 // 
 export default router;
